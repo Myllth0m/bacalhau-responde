ff4ac964916a0ff2d6e98ccca3f2b8985fd24984
Milton D. Yama <miltondyama@gmail.com>
2021-08-24 23:13:17 -0400

    criacao da controller de autenticacao e loginViewModel

---
+++ BacalhauResponde/Controllers/AutenticacaoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using BacalhauResponde.Models;
+using BacalhauResponde.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BacalhauResponde.Controllers
+{
+    public class AutenticacaoController : Controller
+    {
+        private readonly UserManager<Usuario> gerenciadorDeUsuario;
+        private readonly SignInManager<Usuario> gerenciadorDeAcesso;
+        public AutenticacaoController(
+            UserManager<Usuario> gerenciadorDeUsuario,
+            SignInManager<Usuario> gerenciadorDeAcesso)
+        {
+            this.gerenciadorDeUsuario = gerenciadorDeUsuario;
+            this.gerenciadorDeAcesso = gerenciadorDeAcesso;
+        }
+
+        public IActionResult Entrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Entrar(LoginViewModel loginViewModel)
+        {
+            var usuario = await gerenciadorDeUsuario.FindByEmailAsync(loginViewModel.Email);
+
+            var resultadoDaTentativaDeLogin = await gerenciadorDeAcesso.PasswordSignInAsync(usuario, loginViewModel.Senha, false, true);
+
+            if (resultadoDaTentativaDeLogin.Succeeded)
+                return RedirectToAction(nameof(Index), nameof(HomeController));
+            else
+            {
+                TempData["ERRO"] = "Verifique se o login e a senha estão corretos";
+                return View(loginViewModel);
+            }
+        }
+
+        public async Task<IActionResult> Sair()
+        {
+            await gerenciadorDeAcesso.SignOutAsync();
+
+            return RedirectToAction(nameof(Entrar));
+        }
+    }
+}
--- BacalhauResponde/Models/ViewModels/LoginViewModel.cs
+++ BacalhauResponde/Models/ViewModels/LoginViewModel.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BacalhauResponde.Models.ViewModels
 {
     public class LoginViewModel
     {
+        [Display(Name = "E-mail")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+
+        [MinLength(6, ErrorMessage = "Senha inválida")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
     }
 }
